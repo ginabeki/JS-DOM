bb143728d0917a3a282335a12e33e895a5680ec0
Gina Beki <ginabeki3@gmail.com>
2022-07-24 08:42:01 +0300

    update

--- DOM/dom.js
+++ DOM/dom.js
@@ -9,3 +9,31 @@ setTimeout(() => document.body.style.background = "", 1000);
 
 // CSSOM for styling
 // stands for CSS Object Model for CSS rules and stylesheets, that explains how they are represented as objects, and how to read and write them 
+
+// WALKING THE DOM
+/**
+ * All operationson on the DOM start with the document object. That's the main entry point to DOM. From it we can access any node 
+ * The topmost tree nodes are available directly as document properties: <html> = document.documentElement
+ * The topmost document node is document.documentElement. That's the DOM node of the <html> tag
+ * Another widely used DOM node is the <body> element - document.body. 
+ * The <head> tag is available as document.head
+ * In the DOM, the null value means doesn't exist or no such node
+ * # CHILDREN: childNodes, firtChild, lastChild
+ * Child nodes (or children) - elements that are directchildren. They ae nested directly in the given one. For instance <head> and <body> are children of <html>
+ * Descendants - all elements that are nested in the given one, including children, their children, and so on.
+ * 
+ * elem.childNodes[0] === elem.firstChild
+ * elem.childNodes[elem.childNodes.length - 1] === elem.lastChild
+ * There's also a special function elem.hasChildNodes() to check whether there are any child nodes.
+ * 
+ * SIBLINGS AND THE PARENT
+ * //parent of <body> is <html>
+ * alert(document.body.parentNode === document.documentElement); // true
+ * 
+ * // after <head> goes <body>
+ * alert(document.head.nextSibling); // HTMLBodyElement
+ * // before <body> goes <head>
+ * alert(document.body.previousSibling); //HTMLHeadElement
+ * For all nodes: parentNode, childNodes, firstChild, lastChild, previousSibling, nextSibling.
+For element nodes only: parentElement, children, firstElementChild, lastElementChild, previousElementSibling, nextElementSibling.
+ */
